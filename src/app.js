13a2219dd4a2d866ad5960edba68c3b629c3e08d
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2024-01-22 19:53:37 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
-function excuseGen() {
+import '../index.scss';
 
-    var Character = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-    var Action = ['ate', 'peed', 'crushed', 'broke'];
-    var Object = ['my homework', 'the keys', 'the car'];
+window.onload = () => {
+    document.querySelector('#btn').addEventListener("click", () => {
+        document.querySelector('#the-excuse').innerHTML = generateExcuse();
+    });
+    console.log('Hello Rigo from the console!');
+};
 
-    var C1 = Character[Math.round(Math.random()*(Character.length-1))];
-    var C2 = Action[Math.round(Math.random()*(Action.length-1))];
-    var C3 = Object[Math.round(Math.random()*(Object.length-1))];
+let generateExcuse = () => {
+    let pronoun = ['A', 'The'];
+    let subject = ['jogger', 'racoon', 'dog', 'driver', 'comedian', 'pincone'];
+    let action = ['took my', 'threw my', 'yelled at my', 'stole my', 'bit my'];
+    let object = ['homework', 'toe', 'car', 'shoe'];
+    let where = ['on the streets', 'in my house', 'in my driveway'];
 
-	document.getElementById('TheExcuses').innerHTML = '<div> ' + C1 + ' ' + C2 + '<br> ' + C3 + '.</div>'
-}
+    let proIndx = Math.floor(Math.random() * pronoun.length);
+    let subjIndx = Math.floor(Math.random() * subject.length);
+    let actionIndx = Math.floor(Math.random() * action.length);
+    let objectIndx = Math.floor(Math.random() * object.length);
+    let whereIndx = Math.floor(Math.random() * where.length);
+
+    return pronoun[proIndx] + ' ' + subject[subjIndx] + ' ' + action[actionIndx] + ' ' + object[objectIndx] + ' ' + where[whereIndx];
+};
